7d49b2dbce94d83724039b6d902122cdec7290b3
Sebastian Chavarria <sebastianchavarria570@gmail.com>
2026-08-04 01:21:52 -0600

    creacion de constructores y get and set

--- src/Modulos/Sentencias.java
+++ src/Modulos/Sentencias.java
@@ -1,4 +1,14 @@
 package Modulos;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class Sentencias {
+    public void save (Tipos env) throws Exception{
+        String sql= "INSERT INTO ENVIOS (Destinatario, peso, envio, paquete, costo) VALUES (?,?,?,?,?)";
+        try(
+                    Connection conn = Conexion.getConnection()
+                    PreparedStatment stmt= conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+                )
+    }
 }
