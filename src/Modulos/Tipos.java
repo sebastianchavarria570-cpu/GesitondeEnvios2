7b7d48ba88afb6b018099e7666384a7d3dce23ad
Sebastian Chavarria <sebastianchavarria570@gmail.com>
2026-08-04 01:16:24 -0600

    creacion de constructores y get and set

--- src/Modulos/Tipos.java
+++ src/Modulos/Tipos.java
@@ -8,4 +8,68 @@ public class Tipos {
     public String paquete;
     public double costo;
 
+    public Tipos(String destinatario, double peso, String envio, String paquete, double costo) {
+        Destinatario = destinatario;
+        this.peso = peso;
+        this.envio = envio;
+        this.paquete = paquete;
+        this.costo = costo;
+    }
+
+    public Tipos(int id, String destinatario, double peso, String envio, String paquete, double costo) {
+        this.id = id;
+        Destinatario = destinatario;
+        this.peso = peso;
+        this.envio = envio;
+        this.paquete = paquete;
+        this.costo = costo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDestinatario() {
+        return Destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        Destinatario = destinatario;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getEnvio() {
+        return envio;
+    }
+
+    public void setEnvio(String envio) {
+        this.envio = envio;
+    }
+
+    public String getPaquete() {
+        return paquete;
+    }
+
+    public void setPaquete(String paquete) {
+        this.paquete = paquete;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
 }
