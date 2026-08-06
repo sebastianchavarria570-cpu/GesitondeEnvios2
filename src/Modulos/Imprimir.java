8a1c01f4825d4110449370de5d373389f89e30ea
Sebastian Chavarria <sebastianchavarria570@gmail.com>
2026-08-06 01:39:37 -0600

    primer metodo para imprimir

--- src/Modulos/Imprimir.java
+++ src/Modulos/Imprimir.java
@@ -1,5 +1,18 @@
 package Modulos;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Imprimir {
+
+    public void imprimirEnvio (Tipos envio){
+        System.out.println("ID "+ envio.getId());
+        System.out.printf("Destinatario "+ envio.getDestinatario());
+        System.out.println(" Peso "+ envio.getPeso());
+        System.out.println(" Envio "+ envio.getEnvio());
+        System.out.println(" Paquete "+ envio.getPaquete());
+        System.out.println(" costo " + envio.getCosto());
+    }
+
+
 
 }
