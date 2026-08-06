fdd03ad40049bfcccc5d605d421205b167d2e5bc
Sebastian Chavarria <sebastianchavarria570@gmail.com>
2026-08-06 02:00:57 -0600

    case 1 y 2 del main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,62 @@
 import Modulos.Imprimir;
 import Modulos.Sentencias;
+import Modulos.Tipos;
 
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static  Scanner sc = new Scanner(System.in);
+    public static int opc;
     public static void main(String[] args) {
 
         Sentencias sentencias = new Sentencias();
         Imprimir imprimir = new Imprimir();
+        boolean continuar = true;
+        try{
+            while(continuar){
+                menu();
+                switch (opc){
+                    case 1:
+                        System.out.println("ingreasa el nombre del destinatario");
+                        String nombre = sc.nextLine();
+                        System.out.println("ingresa el peso del envio");
+                        double peso = sc.nextDouble();
+                        sc.nextLine();
+                        System.out.println("ingresa el tipo de envio");
+                        String envio = sc.nextLine();
+                        System.out.println("ingresa el tipo de paquete");
+                        String paquete = sc.nextLine();
+                        System.out.printf("ingresa el costo del envio");
+                        double costo = sc.nextDouble();
+
+                        Tipos nuevoEnvio = new Tipos(nombre, peso, envio, paquete, costo);
+                        boolean save =sentencias.save(nuevoEnvio);
+                        if(save == true){
+                            System.out.println("Se ha guardado el envio");
+                        }else {
+                            System.out.println("No se ha guardado el envio");
+                        }
+                        break;
+                        case 2:
+                            System.out.println(" todos los envios");
+                            var todo = sentencias.mostrarTodos();
+                            imprimir.imprimirLista(todo);
+                            break;
+                    case 3:
+                        System.out.println("ingresa el peso");
+                        double peso2 = sc.nextDouble();
+                        var mayorpeso = sentencias.mostrarEnviosMayoresAPeso(peso2);
+                                imprimir.imprimirLista(mayorpeso);
+                }
+
+            }
 
 
 
+        }catch(Exception e){
+            System.out.println("Error");
+        }
 
 
     }
@@ -26,8 +70,7 @@ public class Main {
         System.out.println("6. Ver conteo por tipo de envio");
         System.out.println("7. Ver total de envios");
         System.out.println("0. Salir");
-        System.out.print("Selecciona una opcion: ");
-        int opc = sc.nextInt();
+        opc = sc.nextInt();
         sc.nextLine();
 
     }
